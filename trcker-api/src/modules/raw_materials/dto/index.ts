c26471497c2e417d422760dd8bfff617da99d7a6
xaxxoo <bnahmad83@gmail.com>
2026-01-08 16:06:59 +0100

    Add index file for exporting raw material DTOs

---
+++ trcker-api/src/modules/raw_materials/dto/index.ts
@@ -0,0 +1,6 @@
+export * from './create-supplier.dto';
+export * from './create-raw-material.dto';
+export * from './update-raw-material.dto';
+export * from './create-batch.dto';
+export * from './receive-batch.dto';
+export * from './raw-material-query.dto';
