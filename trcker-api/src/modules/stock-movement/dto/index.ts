59e5da8c3a3a69d72d53c1af08f599e84b8bcd59
xaxxoo <bnahmad83@gmail.com>
2026-01-09 18:39:10 +0100

    feat(stock-movement): add index file to export stock movement DTOs

---
+++ trcker-api/src/modules/stock-movement/dto/index.ts
@@ -0,0 +1,7 @@
+export * from './create-receipt.dto';
+export * from './create-issue.dto';
+export * from './create-transfer.dto';
+export * from './create-adjustment.dto';
+export * from './create-reservation.dto';
+export * from './update-movement.dto';
+export * from './movement-query.dto';
