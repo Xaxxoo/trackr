38a852b841b53a300e8ff64a1ea70c465ed6d957
xaxxoo <bnahmad83@gmail.com>
2026-01-09 10:06:11 +0100

    feat(production): add index file for exporting production DTOs

---
+++ trcker-api/src/modules/production/dto/index.ts
@@ -0,0 +1,6 @@
+export * from './create-production-order.dto';
+export * from './update-production-order.dto';
+export * from './start-production.dto';
+export * from './complete-production.dto';
+export * from './record-consumption.dto';
+export * from './production-order-query.dto';
