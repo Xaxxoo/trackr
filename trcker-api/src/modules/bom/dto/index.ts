bc8215df720b74c678e58371139704846ec732a9
xaxxoo <bnahmad83@gmail.com>
2026-01-09 08:29:53 +0100

    Add index file for BOM DTO exports

---
+++ trcker-api/src/modules/bom/dto/index.ts
@@ -0,0 +1,6 @@
+export * from './create-bom.dto';
+export * from './update-bom.dto';
+export * from './add-bom-item.dto';
+export * from './update-bom-item.dto';
+export * from './create-bom-version.dto';
+export * from './bom-query.dto';
