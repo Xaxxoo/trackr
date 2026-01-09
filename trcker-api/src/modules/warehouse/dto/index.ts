7718b179d915f13491d3c97ca3ddc9de89cb3dda
xaxxoo <bnahmad83@gmail.com>
2026-01-09 12:45:15 +0100

    feat(warehouse): create index file to export DTOs for warehouse module

---
+++ trcker-api/src/modules/warehouse/dto/index.ts
@@ -0,0 +1,7 @@
+export * from './create-warehouse.dto';
+export * from './update-warehouse.dto';
+export * from './create-location.dto';
+export * from './update-location.dto';
+export * from './create-stock-movement.dto';
+export * from './warehouse-query.dto';
+export * from './stock-query.dto';
